6ceaf488821bf54f0b2e9165bd5168c74846bb5e
Héctor Fabio Cadavid Rengifo <hector.cadavid@gmail.com>
2017-09-06 16:01:19 -0500

    @ComponentScan annotation included

--- src/main/java/edu/eci/arsw/blueprintsapi/BlueprintsAPIApplication.java
+++ src/main/java/edu/eci/arsw/blueprintsapi/BlueprintsAPIApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"edu.eci.arsw.blueprints"})
 public class BlueprintsAPIApplication {
 
 	public static void main(String[] args) {
