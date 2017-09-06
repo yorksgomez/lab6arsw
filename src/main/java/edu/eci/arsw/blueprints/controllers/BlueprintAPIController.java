7490c5e36a568f8e5bef9192d1c972a3b60502d2
Héctor Cadavid <hector.cadavid@gmail.com>
2017-09-06 10:50:24 -0500

    first commit

--- src/main/java/edu/eci/arsw/blueprints/controllers/BlueprintAPIController.java
+++ src/main/java/edu/eci/arsw/blueprints/controllers/BlueprintAPIController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
  *
  * @author hcadavid
  */
-public class BlueprintController {
+public class BlueprintAPIController {
     
     
     
