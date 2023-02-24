0cf419d68c86a4670ab40ae849c9c561fce4725f
Yorks Gomez <yorksgomez@gmail.com>
2023-02-24 11:30:46 -0500

    arreglos iniciales

--- src/test/java/edu/eci/arsw/blueprints/test/services/ApplicationServicesTests.java
+++ src/test/java/edu/eci/arsw/blueprints/test/services/ApplicationServicesTests.java
@@ -9,8 +9,10 @@ import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import edu.eci.arsw.blueprintsapi.BlueprintsAPIApplication;
+
 @RunWith(SpringRunner.class)
-@SpringBootTest()
+@SpringBootTest(classes={BlueprintsAPIApplication.class})
 public class ApplicationServicesTests {
 
     
