97d42ca14da7df1df77bc38bb818016324d11dec
rayo100 <cv.1000458640@gmail.com>
2023-03-14 22:10:50 -0500

    Adicionando front-end vista y lógico

--- src/main/resources/static/js/apimock.js
+++ src/main/resources/static/js/apimock.js
@@ -4,11 +4,17 @@ apimock=(function(){
 
 	var mockdata=[];
 
-	mockdata["johnconnor"]=	[{author:"johnconnor","points":[{"x":150,"y":120},{"x":215,"y":115}],"name":"house"},
-	 {author:"johnconnor","points":[{"x":340,"y":240},{"x":15,"y":215}],"name":"gear"}];
-	 
-	mockdata["maryweyland"]=[{author:"maryweyland","points":[{"x":140,"y":140},{"x":115,"y":115}],"name":"house2"},
-	 {author:"maryweyland","points":[{"x":140,"y":140},{"x":115,"y":115}],"name":"gear2"}];
+	mockdata["johnconnor"]=	[
+		{author:"johnconnor","points":[{"x":150,"y":120},{"x":215,"y":115}],"name":"house"},
+	 	{author:"johnconnor","points":[{"x":340,"y":240},{"x":15,"y":215}],"name":"gear"},
+		{author:"johnconnor","points":[{"x":34,"y":76},{"x":190,"y":71}],"name":"tree"},
+		{author:"johnconnor","points":[{"x":370,"y":210},{"x":152,"y":212}],"name":"car"}];
+
+	mockdata["maryweyland"]=[
+		{author:"maryweyland","points":[{"x":140,"y":140},{"x":115,"y":115}],"name":"house2"},
+	 	{author:"maryweyland","points":[{"x":140,"y":140},{"x":115,"y":115}],"name":"gear2"},
+		{author:"maryweyland","points":[{"x":120,"y":120},{"x":115,"y":125}],"name":"tree2"},
+		{author:"maryweyland","points":[{"x":130,"y":111},{"x":125,"y":135}],"name":"car2"}];
 
 
 	return {
--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -1,20 +1,75 @@
-var App = (() => {
-    let _selectedAuthor = "";
-    let _selectedBlueprints = [];
+var apimock = apimock;
 
-    return {
-        selectAuthor: (author) => {
-            _selectedAuthor = author;
-            apimock.getBlueprintsByAuthor(author, (blueprints) => _selectedBlueprints = blueprints);
-        },
-        getAuthor: () => {
-            return _selectedAuthor;
-        },
-        getBlueprints: () => {
-            return _selectedBlueprints;
-        },
-        updatePoints: () => {
-            
+var app = (function (){
+    var author;
+    var blueprintName;
+
+    function getName() {
+            $("#name").text(author + "'s " + "blueprints:");
         }
-    };
+
+     function getNameAuthorBlueprints() {
+        author = $("#author").val();
+        if (author === "") {
+            alert("Debe ingresar un nombre");
+        } else {
+            apimock.getBlueprintsByAuthor(author,authorData);
+        }
+     }
+
+     var authorData = function( data) {
+         $("#table tbody").empty();
+         if (data === undefined) {
+             alert("No existe el autor");
+             $("#name").empty();
+             $("#points").text("Total Points");
+             $("#nameblu").empty();
+         } else {
+             getName();
+             const datanew = data.map((elemento) => {
+                 return {
+                     name: elemento.name,
+                     puntos: elemento.points.length
+                 }
+             });
+
+             datanew.map((elementos) => {
+                 $("#table > tbody:last").append($("<tr><td>" + elementos.name + "</td><td>" + elementos.puntos.toString() +
+                     "</td><td>" + "<button  id=" + elementos.name + " onclick=app.getBlueprintByAuthorAndName(this)>open</button>" + "</td>"));
+             });
+
+             const totalPuntos = datanew.reduce((suma, {puntos}) => suma + puntos, 0);
+
+             $("#points").text("Total user points: " + totalPuntos);
+            }
+         }
+
+         function getBlueprintByAuthorAndName(data) {
+                 author = $("#author").val();
+                 blueprintName = data.id;
+                 $("#nameblu").text("Current blueprint: " + blueprintName);
+                 apimock.getBlueprintByAuthorAndName(author, blueprintName, printPoints);
+             }
+
+         function printPoints(data) {
+                 const puntos = data.points;
+                 var c = document.getElementById("myCanvas");
+                 var ctx = c.getContext("2d");
+                 ctx.clearRect(0, 0, c.width, c.height);
+                 ctx.restore();
+                 ctx.beginPath();
+                 for (let i = 1; i < puntos.length; i++) {
+                     ctx.moveTo(puntos[i - 1].x, puntos[i - 1].y);
+                     ctx.lineTo(puntos[i].x, puntos[i].y);
+                     if (i === puntos.length - 1) {
+                         ctx.moveTo(puntos[i].x, puntos[i].y);
+                         ctx.lineTo(puntos[0].x, puntos[0].y);
+                     }
+                 }
+                 ctx.stroke();
+             }
+     return{
+        getBlueprintByAuthorAndName:getBlueprintByAuthorAndName,
+        getNameAuthorBlueprints: getNameAuthorBlueprints
+     }
 })();
