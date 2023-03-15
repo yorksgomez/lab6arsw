5ffb923c23f0d23cbff65721c958a55387045187
Yorks Gomez <yorksgomez@gmail.com>
2023-03-14 22:35:00 -0500

    canvas

--- src/main/resources/static/js/apimock.js
+++ src/main/resources/static/js/apimock.js
@@ -8,7 +8,7 @@ apimock=(function(){
 	 {author:"johnconnor","points":[{"x":340,"y":240},{"x":15,"y":215}],"name":"gear"}];
 	 
 	mockdata["maryweyland"]=[{author:"maryweyland","points":[{"x":140,"y":140},{"x":115,"y":115}],"name":"house2"},
-	 {author:"maryweyland","points":[{"x":140,"y":140},{"x":115,"y":115}],"name":"gear2"}];
+	 {author:"maryweyland","points":[{"x":340,"y":340},{"x":115,"y":115}],"name":"gear2"}];
 
 
 	return {
--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -15,6 +15,38 @@ var App = (() => {
         },
         updatePoints: () => {
             
+        },
+        draw: (blueprint) => {
+            
+            apimock.getBlueprintsByNameAndAuthor(_selectedAuthor, blueprint, (found) => {
+                let canvas = document.getElementById("myCanvas");
+                let context = canvas.getContext('2d');
+                let points = found.points;
+
+                context.lineWidth = 5;
+                context.strokeStyle = "black";
+                context.fillStyle = "black";
+
+                if(points == 0) return;
+
+                if(points == 1) {
+                    let p = points[0];
+                    context.fillRect(p.x, p.y, 5, 5);
+                    return;
+                }
+
+                for(let i = 1; i < points.length; i++) {
+                    let p1 = points[i];
+                    let p2 = points[i-1];
+                    context.beginPath();
+                    console.log(p1);
+                    console.log(p2);
+                    context.moveTo(p1.x, p1.y);
+                    context.lineTo(p2.x, p2.y);
+                    context.stroke();
+                }
+            });
+
         }
     };
 })();
